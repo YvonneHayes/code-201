7c147c123c6933c0a6c0a4a30b19a236f0bb51f7
Johnny Luangphasy <johnny@jluangphasy.com>
2016-03-21 14:13:43 -0700

    Added test and function for problem 2

--- lab-mar-21/scripts/compute.js
+++ lab-mar-21/scripts/compute.js
@@ -10,3 +10,14 @@ function sum(num1, num2) {
 }
 
 exports.sum = sum;
+
+// Problem 2
+function multiply(num1, num2) {
+  var result = num1 * num2;
+
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + result + '.');
+
+  return result;
+}
+
+exports.multiply = multiply;
--- lab-mar-21/tests/test.js
+++ lab-mar-21/tests/test.js
@@ -1,12 +1,22 @@
 var assert = require('assert');
 var compute = require('../scripts/compute.js');
 
-describe('Problem 1: Sum of two numbers', function() {
+describe('Problem 1: sum of two numbers', function() {
   it('should add 1 and 6 to get 7', function() {
     assert(7 == compute.sum(1, 6));
   });
 
-  it('should add 10 and 78 not get 68', function() {
+  it('should add 10 and 78 to not get 68', function() {
     assert(68 != compute.sum(10, 78));
+  });
+});
+
+describe('Problem 2: product of two numbers', function () {
+  it('should multiply 2 and 9 to get 18', function () {
+    assert(18 == compute.multiply(2, 9));
+  });
+
+  it('should multiply 7 and 4 to not get 21', function () {
+    assert(21 != compute.multiply(7, 4));
   });
 });
