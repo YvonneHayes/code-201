aba19676bea1730f6cbb76a62f6f1d3e09ad4454
Johnny Luangphasy <johnny@jluangphasy.com>
2016-03-21 15:16:02 -0700

    Added test and function for problem 3, changed function names for problem 1 and 2

--- lab-mar-21/tests/test.js
+++ lab-mar-21/tests/test.js
@@ -3,26 +3,26 @@ var compute = require('../scripts/compute.js');
 
 describe('Problem 1: sum of two numbers', function() {
   it('should add 1 and 6 to get 7', function() {
-    assert(7 == compute.sum(1, 6));
+    assert(7 == compute.addNum(1, 6));
   });
 
   it('should add 10 and 78 to not get 68', function() {
-    assert(68 != compute.sum(10, 78));
+    assert(68 != compute.addNum(10, 78));
   });
 });
 
 describe('Problem 2: product of two numbers', function () {
   it('should multiply 2 and 9 to get 18', function () {
-    assert(18 == compute.multiply(2, 9));
+    assert(18 == compute.multiplyNum(2, 9));
   });
 
   it('should multiply 7 and 4 to not get 21', function () {
-    assert(21 != compute.multiply(7, 4));
+    assert(21 != compute.multiplyNum(7, 4));
   });
 });
 
 describe('Problem 3: sum and product of three numbers', function () {
-  var result = compute.sumAndMultiply(1, 2, 3);
+  var result = compute.sumAndProduct(1, 2, 3);
 
   it('should add 1, 2, and 3 to get 6', function () {
     assert(6 == result[0]);
