094a1741f40daad3f1dc6dbd837c193eca4afba9
Johnny Luangphasy <johnny@jluangphasy.com>
2016-03-22 10:54:45 -0700

    Setup some of the main files

---
+++ pizza-3001/scripts/app.js
@@ -0,0 +1,11 @@
+var exports = {}; // Dummy declaration to let this file co-exist with test.js
+
+var pAddFiveIn  = document.getElementById('pAddFiveIn');
+var pAddFiveOut = document.getElementById('pAddFiveOut');
+
+var arg = 10;
+
+document.addEventListener('DOMContentLoaded', function() {
+  pAddFiveIn.textContent = 'Calling addFive() with an argument of ' + arg;
+  pAddFiveOut.textContent = 'addFive() returned ' + addFive(arg);
+});
---
+++ pizza-3001/scripts/compute.js
@@ -0,0 +1,13 @@
+var question = ['first', 'second', 'third'];
+
+function getQuestion(index) {
+  return (question[index]);
+}
+
+exports.getQuestion = getQuestion;
+
+function addFive(num) {
+  return num + 5;
+}
+
+exports.addFive = addFive;
