eac008329fe1e07f0d5ac8a8c142b95ecd53299b
Johnny Luangphasy <johnny@jluangphasy.com>
2016-03-22 11:26:10 -0700

    Setup pages

--- pizza-3001/scripts/app.js
+++ pizza-3001/scripts/app.js
@@ -1,11 +1,11 @@
-var exports = {}; // Dummy declaration to let this file co-exist with test.js
+var exports = {};
 
-var pAddFiveIn  = document.getElementById('pAddFiveIn');
-var pAddFiveOut = document.getElementById('pAddFiveOut');
-
-var arg = 10;
+// var pAddFiveIn  = document.getElementById('pAddFiveIn');
+// var pAddFiveOut = document.getElementById('pAddFiveOut');
+//
+// var arg = 10;
 
 document.addEventListener('DOMContentLoaded', function() {
-  pAddFiveIn.textContent = 'Calling addFive() with an argument of ' + arg;
-  pAddFiveOut.textContent = 'addFive() returned ' + addFive(arg);
+  // pAddFiveIn.textContent = 'Calling addFive() with an argument of ' + arg;
+  // pAddFiveOut.textContent = 'addFive() returned ' + addFive(arg);
 });
