4364e3c369aac5e5ee9e76f16f4b44c3b89105d9
Johnny Luangphasy <johnny@jluangphasy.com>
2016-03-24 11:44:32 -0700

    Started moving functions into another JS, comments in data JS

--- pizza-3001/scripts/app.js
+++ pizza-3001/scripts/app.js
@@ -3,12 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var elHome = document.getElementById('content-home');
   var elSales = document.getElementById('content-sales-data');
 
-  function Store(locationName, dayHours, minMax) {
-    this.locationName = locationName;
-    this.dayHours = dayHours;
-    this.minMax = minMax;
-  }
-
   var storeBeaverton = new Store('Beaverton', beavertonDayHours, beavertonMinMax);
   console.log('Beaverton Object', storeBeaverton);
 
@@ -26,122 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var storeAirport = new Store('PDX Airport', airportHours, airportMinMax);
   console.log('PDX Airport Object', storeAirport);
-
-  function convertNumberTo12Hour(number) {
-    if (number <= 0 || number > 24) {
-      return 'Enter a number between 0 and 25';
-    }
-
-    var string = '';
-    var newNumber = number;
-
-    if (number > 12) {
-      newNumber = number - 12;
-    }
-
-    string += newNumber + ':00 ';
-
-    if (number == 24 || number < 12) {
-      string += 'AM';
-    } else {
-      string += 'PM';
-    }
-
-    return string;
-  }
-
-  function generateRandomNumber(minNumber, maxNumber) {
-    var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-
-    // console.log('The random number for a min number of ' + minNumber + ' and max number of ' + maxNumber + ' is ' + randomNumber + '.');
-
-    return randomNumber;
-  }
-
-  function createEl(elName, elText, elAttribute) {
-    var el = document.createElement(elName);
-
-    if (elText === 0) {
-      elText = elText.toString();
-    }
-
-    if (elText) {
-      el.textContent = elText;
-    }
-
-    if (elAttribute) {
-      el.setAttribute(elAttribute.name, elAttribute.value);
-    }
-
-    return el;
-  }
-
-  function getMinAndMax(time) {
-    switch (true) {
-      // 8am-11am
-      case (time > 7 && time < 11):
-        return { pizzaMin: 0, pizzaMax: 4, deliveryMin: 0, deliveryMax: 4 };
-        break;
-      // 11am-2pm
-      case (time > 10 && time < 14):
-        return { pizzaMin: 0, pizzaMax: 7, deliveryMin: 0, deliveryMax: 4 };
-        break;
-      // 2pm-5pm
-      case (time > 13 && time < 17):
-        return { pizzaMin: 2, pizzaMax: 15, deliveryMin: 1, deliveryMax: 4 };
-        break;
-      // 5pm-8pm
-      case (time > 16 && time < 20):
-        return { pizzaMin: 15, pizzaMax: 35, deliveryMin: 3, deliveryMax: 8 };
-        break;
-      // 8pm-11pm
-      case (time > 19 && time < 23):
-        return { pizzaMin: 12, pizzaMax: 31, deliveryMin: 5, deliveryMax: 12 };
-        break;
-      // 11pm-2am
-      case ((time > 0 && time < 2) || (time > 22 && time < 25)):
-        return { pizzaMin: 5, pizzaMax: 20, deliveryMin: 6, deliveryMax: 11 }
-        break;
-    }
-  }
-
-  function getDrivers(deliveryRuns) {
-    return Math.ceil(deliveryRuns / 3);
-  }
-
-  function addClass(idName, classNameAdd) {
-    var el = document.getElementById(idName);
-    var classNames = el.className.split(' ');
-    var newClassNames = [];
-
-    for (var i = 0; i < classNames.length; i++) {
-      if (classNames[i] !== classNameAdd) {
-        newClassNames.push(classNames[i]);
-      }
-    }
-
-    newClassNames.push(classNameAdd);
-
-    el.className = newClassNames.join(' ');
-
-    return newClassNames;
-  }
-
-  function removeClass(idName, classNameRemove) {
-    var el = document.getElementById(idName);
-    var classNames = el.className.split(' ');
-    var newClassNames = [];
-
-    for (var i = 0; i < classNames.length; i++) {
-      if (classNames[i] !== classNameRemove) {
-        newClassNames.push(classNames[i]);
-      }
-    }
-
-    el.className = newClassNames.join(' ');
-
-    return newClassNames;
-  }
 
   function createLocationTable(store) {
     var pizzaOdysseys = 'happy Pizza Odysseys this week for ' + store.locationName + '!';
@@ -297,8 +175,8 @@ document.addEventListener('DOMContentLoaded', function() {
         removeClass('bg-image', 'bg-image--02');
 
         // Show content for sales
-        addClass('home', 'show');
-        removeClass('sales-data', 'show');
+        addClass('content-home', 'show');
+        removeClass('content-sales-data', 'show');
 
         break;
       case 'sales-data':
@@ -312,8 +190,8 @@ document.addEventListener('DOMContentLoaded', function() {
         removeClass('bg-image', 'bg-image--01');
 
         // Show content for sales
-        addClass('sales-data', 'show');
-        removeClass('home', 'show');
+        addClass('content-sales-data', 'show');
+        removeClass('content-home', 'show');
 
         break;
     }
--- pizza-3001/scripts/data.js
+++ pizza-3001/scripts/data.js
@@ -3,115 +3,115 @@ var textTitle = 'Pizza 3001';
 var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 var beavertonDayHours = [
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 0, timeEnd: 0 }
+  { timeStart: 8, timeEnd: 17 }, // Monday
+  { timeStart: 8, timeEnd: 17 }, // Tuesday
+  { timeStart: 8, timeEnd: 17 }, // Wednesday
+  { timeStart: 8, timeEnd: 17 }, // Thursday
+  { timeStart: 8, timeEnd: 17 }, // Friday
+  { timeStart: 0, timeEnd: 0  }, // Saturday
+  { timeStart: 0, timeEnd: 0  }  // Sunday
 ];
 
 var beavertonMinMax = [
-  { minPizza: 0, maxPizza: 4, minDelivery: 0, maxDelivery: 0 },
-  { minPizza: 0, maxPizza: 7, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 2, maxPizza: 15, minDelivery: 1, maxDelivery: 4 },
-  { minPizza: 15, maxPizza: 35, minDelivery: 3, maxDelivery: 8 },
-  { minPizza: 12, maxPizza: 31, minDelivery: 5, maxDelivery: 12 },
-  { minPizza: 5, maxPizza: 20, minDelivery: 6, maxDelivery: 11 }
+  { minPizza: 0,  maxPizza: 4,  minDelivery: 0, maxDelivery: 0  }, // 8am-11am
+  { minPizza: 0,  maxPizza: 7,  minDelivery: 0, maxDelivery: 4  }, // 11am-2pm
+  { minPizza: 2,  maxPizza: 15, minDelivery: 1, maxDelivery: 4  }, // 2pm-5pm
+  { minPizza: 15, maxPizza: 35, minDelivery: 3, maxDelivery: 8  }, // 5pm-8pm
+  { minPizza: 12, maxPizza: 31, minDelivery: 5, maxDelivery: 12 }, // 8pm-11pm
+  { minPizza: 5,  maxPizza: 20, minDelivery: 6, maxDelivery: 11 }  // 11pm-2am
 ];
 
 var hillsboroDayHours = [
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 8, timeEnd: 17 },
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 0, timeEnd: 0 }
+  { timeStart: 8, timeEnd: 17 }, // Monday
+  { timeStart: 8, timeEnd: 17 }, // Tuesday
+  { timeStart: 8, timeEnd: 17 }, // Wednesday
+  { timeStart: 8, timeEnd: 17 }, // Thursday
+  { timeStart: 8, timeEnd: 17 }, // Friday
+  { timeStart: 0, timeEnd: 0  }, // Saturday
+  { timeStart: 0, timeEnd: 0  }  // Sunday
 ];
 
 var hillsboroMinMax = [
-  { minPizza: 1, maxPizza: 3, minDelivery: 1, maxDelivery: 7 },
-  { minPizza: 5, maxPizza: 9, minDelivery: 2, maxDelivery: 8 },
-  { minPizza: 2, maxPizza: 13, minDelivery: 1, maxDelivery: 6 },
-  { minPizza: 18, maxPizza: 32, minDelivery: 3, maxDelivery: 9 },
-  { minPizza: 1, maxPizza: 3, minDelivery: 5, maxDelivery: 12 },
-  { minPizza: 8, maxPizza: 20, minDelivery: 6, maxDelivery: 16 }
+  { minPizza: 1,  maxPizza: 3,  minDelivery: 1, maxDelivery: 7  }, // 8am-11am
+  { minPizza: 5,  maxPizza: 9,  minDelivery: 2, maxDelivery: 8  }, // 11am-2pm
+  { minPizza: 2,  maxPizza: 13, minDelivery: 1, maxDelivery: 6  }, // 2pm-5pm
+  { minPizza: 18, maxPizza: 32, minDelivery: 3, maxDelivery: 9  }, // 5pm-8pm
+  { minPizza: 1,  maxPizza: 3,  minDelivery: 5, maxDelivery: 12 }, // 8pm-11pm
+  { minPizza: 8,  maxPizza: 20, minDelivery: 6, maxDelivery: 16 }  // 11pm-2am
 ];
 
 var downtownDayHours = [
-  { timeStart: 8, timeEnd: 15 },
-  { timeStart: 8, timeEnd: 15 },
-  { timeStart: 8, timeEnd: 15 },
-  { timeStart: 8, timeEnd: 15 },
-  { timeStart: 8, timeEnd: 15 },
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 0, timeEnd: 0 }
+  { timeStart: 8, timeEnd: 15 }, // Monday
+  { timeStart: 8, timeEnd: 15 }, // Tuesday
+  { timeStart: 8, timeEnd: 15 }, // Wednesday
+  { timeStart: 8, timeEnd: 15 }, // Thursday
+  { timeStart: 8, timeEnd: 15 }, // Friday
+  { timeStart: 0, timeEnd: 0  }, // Saturday
+  { timeStart: 0, timeEnd: 0  }  // Sunday
 ];
 
 var downtownMinMax = [
-  { minPizza: 0, maxPizza: 4, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 0, maxPizza: 7, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 2, maxPizza: 15, minDelivery: 1, maxDelivery: 4 },
-  { minPizza: 10, maxPizza: 26, minDelivery: 4, maxDelivery: 6 },
-  { minPizza: 8, maxPizza: 22, minDelivery: 7, maxDelivery: 15 },
-  { minPizza: 0, maxPizza: 2, minDelivery: 2, maxDelivery: 8 }
+  { minPizza: 0,  maxPizza: 4,  minDelivery: 0, maxDelivery: 4  }, // 8am-11am
+  { minPizza: 0,  maxPizza: 7,  minDelivery: 0, maxDelivery: 4  }, // 11am-2pm
+  { minPizza: 2,  maxPizza: 15, minDelivery: 1, maxDelivery: 4  }, // 2pm-5pm
+  { minPizza: 10, maxPizza: 26, minDelivery: 4, maxDelivery: 6  }, // 5pm-8pm
+  { minPizza: 8,  maxPizza: 22, minDelivery: 7, maxDelivery: 15 }, // 8pm-11pm
+  { minPizza: 0,  maxPizza: 2,  minDelivery: 2, maxDelivery: 8  }  // 11pm-2am
 ];
 
 var northEastHours = [
-  { timeStart: 8, timeEnd: 14 },
-  { timeStart: 8, timeEnd: 14 },
-  { timeStart: 8, timeEnd: 14 },
-  { timeStart: 8, timeEnd: 14 },
-  { timeStart: 8, timeEnd: 14 },
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 0, timeEnd: 0 }
+  { timeStart: 8, timeEnd: 14 }, // Monday
+  { timeStart: 8, timeEnd: 14 }, // Tuesday
+  { timeStart: 8, timeEnd: 14 }, // Wednesday
+  { timeStart: 8, timeEnd: 14 }, // Thursday
+  { timeStart: 8, timeEnd: 14 }, // Friday
+  { timeStart: 0, timeEnd: 0  }, // Saturday
+  { timeStart: 0, timeEnd: 0  }  // Sunday
 ];
 
 var northEastMinMax = [
-  { minPizza: 0, maxPizza: 4, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 0, maxPizza: 7, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 5, maxPizza: 15, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 25, maxPizza: 39, minDelivery: 13, maxDelivery: 18 },
-  { minPizza: 22, maxPizza: 36, minDelivery: 5, maxDelivery: 22 },
-  { minPizza: 5, maxPizza: 21, minDelivery: 16, maxDelivery: 31 }
+  { minPizza: 0,  maxPizza: 4,  minDelivery: 0,  maxDelivery: 4  }, // 8am-11am
+  { minPizza: 0,  maxPizza: 7,  minDelivery: 0,  maxDelivery: 4  }, // 11am-2pm
+  { minPizza: 5,  maxPizza: 15, minDelivery: 0,  maxDelivery: 4  }, // 2pm-5pm
+  { minPizza: 25, maxPizza: 39, minDelivery: 13, maxDelivery: 18 }, // 5pm-8pm
+  { minPizza: 22, maxPizza: 36, minDelivery: 5,  maxDelivery: 22 }, // 8pm-11pm
+  { minPizza: 5,  maxPizza: 21, minDelivery: 16, maxDelivery: 31 }  // 11pm-2am
 ];
 
 var clackamasHours = [
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 9, timeEnd: 18 },
-  { timeStart: 9, timeEnd: 18 },
-  { timeStart: 9, timeEnd: 18 },
-  { timeStart: 9, timeEnd: 18 },
-  { timeStart: 9, timeEnd: 15 },
-  { timeStart: 0, timeEnd: 0 }
+  { timeStart: 0, timeEnd: 0  }, // Monday
+  { timeStart: 9, timeEnd: 18 }, // Tuesday
+  { timeStart: 9, timeEnd: 18 }, // Wednesday
+  { timeStart: 9, timeEnd: 18 }, // Thursday
+  { timeStart: 9, timeEnd: 18 }, // Friday
+  { timeStart: 9, timeEnd: 15 }, // Saturday
+  { timeStart: 0, timeEnd: 0  }  // Sunday
 ];
 
 var clackamasMinMax = [
-  { minPizza: 2, maxPizza: 7, minDelivery: 3, maxDelivery: 5 },
-  { minPizza: 3, maxPizza: 8, minDelivery: 3, maxDelivery: 9 },
-  { minPizza: 1, maxPizza: 5, minDelivery: 1, maxDelivery: 4 },
-  { minPizza: 5, maxPizza: 13, minDelivery: 2, maxDelivery: 4 },
-  { minPizza: 22, maxPizza: 41, minDelivery: 15, maxDelivery: 42 },
-  { minPizza: 15, maxPizza: 20, minDelivery: 6, maxDelivery: 21 }
+  { minPizza: 2,  maxPizza: 7,  minDelivery: 3,  maxDelivery: 5  }, // 8am-11am
+  { minPizza: 3,  maxPizza: 8,  minDelivery: 3,  maxDelivery: 9  }, // 11am-2pm
+  { minPizza: 1,  maxPizza: 5,  minDelivery: 1,  maxDelivery: 4  }, // 2pm-5pm
+  { minPizza: 5,  maxPizza: 13, minDelivery: 2,  maxDelivery: 4  }, // 5pm-8pm
+  { minPizza: 22, maxPizza: 41, minDelivery: 15, maxDelivery: 42 }, // 8pm-11pm
+  { minPizza: 15, maxPizza: 20, minDelivery: 6,  maxDelivery: 21 }  // 11pm-2am
 ];
 
 var airportHours = [
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 10, timeEnd: 21 },
-  { timeStart: 10, timeEnd: 21 },
-  { timeStart: 10, timeEnd: 21 },
-  { timeStart: 0, timeEnd: 0 },
-  { timeStart: 0, timeEnd: 0 }
+  { timeStart: 0,  timeEnd: 0  }, // Monday
+  { timeStart: 0,  timeEnd: 0  }, // Tuesday
+  { timeStart: 10, timeEnd: 21 }, // Wednesday
+  { timeStart: 10, timeEnd: 21 }, // Thursday
+  { timeStart: 10, timeEnd: 21 }, // Friday
+  { timeStart: 0,  timeEnd: 0  }, // Saturday
+  { timeStart: 0,  timeEnd: 0  }  // Sunday
 ];
 
 var airportMinMax = [
-  { minPizza: 0, maxPizza: 4, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 0, maxPizza: 7, minDelivery: 0, maxDelivery: 4 },
-  { minPizza: 2, maxPizza: 15, minDelivery: 1, maxDelivery: 4 },
-  { minPizza: 6, maxPizza: 9, minDelivery: 5, maxDelivery: 18 },
-  { minPizza: 4, maxPizza: 8, minDelivery: 2, maxDelivery: 5 },
-  { minPizza: 2, maxPizza: 4, minDelivery: 3, maxDelivery: 11 }
+  { minPizza: 0, maxPizza: 4,  minDelivery: 0, maxDelivery: 4  }, // 8am-11am
+  { minPizza: 0, maxPizza: 7,  minDelivery: 0, maxDelivery: 4  }, // 11am-2pm
+  { minPizza: 2, maxPizza: 15, minDelivery: 1, maxDelivery: 4  }, // 2pm-5pm
+  { minPizza: 6, maxPizza: 9,  minDelivery: 5, maxDelivery: 18 }, // 5pm-8pm
+  { minPizza: 4, maxPizza: 8,  minDelivery: 2, maxDelivery: 5  }, // 8pm-11pm
+  { minPizza: 2, maxPizza: 4,  minDelivery: 3, maxDelivery: 11 }  // 11pm-2am
 ];
